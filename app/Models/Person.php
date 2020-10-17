6dd5579ec92ce977b009a5eabc3dd9c1ab9df145
Adam Lundell <U1853373@unimail.hud.ac.uk>
2020-10-17 03:37:05 +0100

    uppdated new pepole page controllers ect

---
+++ app/Models/Person.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+
+
+    use HasFactory;
+
+    public function getPathAttribute(){
+        return $this ->path();
+    }
+    public function path (){
+
+        return '/people/' . $this->id;
+    }
+}
