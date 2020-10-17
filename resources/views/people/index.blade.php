15c165a4727ca1e2e9f79bf1b661a20da9f18253
Adam Lundell <U1853373@unimail.hud.ac.uk>
2020-10-17 03:20:13 +0100

    uppdated new pepole page controllers ect

---
+++ resources/views/people/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="sm:container sm:mx-auto sm:mt-10">
+        <div class="w-full sm:px-6">
+            <ul>
+                @foreach($people as $p)
+                <li>
+                  <a href="{{$p ->path}}" > {{$p->Name}}</a>
+                </li>
+                    @endforeach
+            </ul>
+
+        </div>
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/people',[\App\Http\Controllers\PersonController::class,'index']);
+
+Route::get('/people/{person}',[\App\Http\Controllers\PersonController::class,'show']);
+
